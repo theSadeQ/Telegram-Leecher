9b7474c9b29b5f371fe77b50ed62176d4e509578
Xron Trix <xrontrix@gmail.com>
2024-11-21 12:31:45 +0530

    refactor: no more need to download image using aria2c

--- colab_leecher/utility/task_manager.py
+++ colab_leecher/utility/task_manager.py
@@ -35,7 +35,6 @@ from colab_leecher.utility.variables import (
     BotTimes,
     Messages,
     Paths,
-    Aria2c,
     Transfer,
     TaskError,
 )
@@ -132,11 +131,6 @@ async def taskScheduler():
         makedirs(Paths.down_path)
     Messages.link_p = str(DUMP_ID)[4:]
 
-    try:
-        system(f"aria2c -d {Paths.WORK_PATH} -o Hero.jpg {Aria2c.pic_dwn_url}")
-    except Exception:
-        Paths.HERO_IMAGE = Paths.DEFAULT_HERO
-
     MSG.sent_msg = await colab_bot.send_message(chat_id=DUMP_ID, text=src_text[0])
 
     if len(src_text) > 1:
@@ -147,10 +141,10 @@ async def taskScheduler():
     Messages.task_msg += f"__[{BOT.Mode.type.capitalize()} {BOT.Mode.mode.capitalize()} as {BOT.Setting.stream_upload}]({Messages.src_link})__\n\n"
 
     await MSG.status_msg.delete()
-    img = Paths.THMB_PATH if ospath.exists(Paths.THMB_PATH) else Paths.HERO_IMAGE
+
     MSG.status_msg = await colab_bot.send_photo(  # type: ignore
         chat_id=OWNER,
-        photo=img,
+        photo="https://picsum.photos/900/600",
         caption=Messages.task_msg
         + Messages.status_head
         + f"\n📝 __Starting DOWNLOAD...__"
