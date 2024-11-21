c07394b5a14d23569c319865fb7827bb068a9c69
Xron Trix <xrontrix@gmail.com>
2024-11-21 15:33:36 +0530

    refactor: moved callback query in other module

--- colab_leecher/__main__.py
+++ colab_leecher/__main__.py
@@ -1,19 +1,18 @@
 # copyright 2024 © Xron Trix | https://github.com/Xrontrix10
 
 
-import logging, os
-from datetime import datetime
-from pyrogram import filters, enums
-from asyncio import sleep, get_event_loop
+import logging
+from asyncio import sleep
+from pyrogram import filters
+from .utility.variables import BOT
 from colab_leecher import colab_bot, OWNER
-from colab_leecher.utility.handler import cancelTask
-from .utility.variables import BOT, MSG, BotTimes, Paths
-from .utility.task_manager import taskScheduler, task_starter
+from .utility.task_manager import task_starter
 from .utility.helper import isLink, setThumbnail, message_deleter
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 # Handler functions
 from .handlers import utils
+from .handlers.callbacks import handle_callbacks
 
 src_request_msg = None
 
@@ -40,7 +39,7 @@ async def help_command(client, message):
     await utils.help_command(message)
 
 
-# ================= Thumbnail Handlers =================
+# =================== Thumbnail Handler =====================
 
 
 # sets thumbnail image
@@ -57,6 +56,75 @@ async def handle_image(client, message):
         )
     await sleep(15)
     await message_deleter(message, msg)
+
+
+
+# ================== Leech Utility Handlers =================
+
+
+# /setname command handler
+@colab_bot.on_message(filters.command("setname") & filters.private)
+async def custom_name(client, message):
+    global BOT
+    if len(message.command) != 2:
+        msg = await message.reply_text(
+            "Send\n/setname <code>custom_fileame.extension</code>\nTo Set Custom File Name 📛",
+            quote=True,
+        )
+    else:
+        BOT.Options.custom_name = message.command[1]
+        msg = await message.reply_text(
+            "Custom Name Has Been Successfully Set !", quote=True
+        )
+
+    await sleep(15)
+    await message_deleter(message, msg)
+
+
+# /zipaswd command handler
+@colab_bot.on_message(filters.command("zipaswd") & filters.private)
+async def zip_pswd(client, message):
+    global BOT
+    if len(message.command) != 2:
+        msg = await message.reply_text(
+            "Send\n/zipaswd <code>password</code>\nTo Set Password for Output Zip File. 🔐",
+            quote=True,
+        )
+    else:
+        BOT.Options.zip_pswd = message.command[1]
+        msg = await message.reply_text(
+            "Zip Password Has Been Successfully Set !", quote=True
+        )
+
+    await sleep(15)
+    await message_deleter(message, msg)
+
+
+# /unzipaswd command handler
+@colab_bot.on_message(filters.command("unzipaswd") & filters.private)
+async def unzip_pswd(client, message):
+    global BOT
+    if len(message.command) != 2:
+        msg = await message.reply_text(
+            "Send\n/unzipaswd <code>password</code>\nTo Set Password for Extracting Archives. 🔓",
+            quote=True,
+        )
+    else:
+        BOT.Options.unzip_pswd = message.command[1]
+        msg = await message.reply_text(
+            "Unzip Password Has Been Successfully Set !", quote=True
+        )
+
+    await sleep(15)
+    await message_deleter(message, msg)
+
+
+# ================== Callback Handlers =================
+
+
+@colab_bot.on_callback_query()
+async def handle_options(client, callback_query):
+    await handle_callbacks(callback_query)
 
 
 @colab_bot.on_message(filters.command("tupload") & filters.private)
@@ -169,256 +237,6 @@ async def handle_url(client, message):
         await message.reply_text(
             "<i>I am Already Working ! Please Wait Until I finish 😣!!</i>"
         )
-
-
-@colab_bot.on_callback_query()
-async def handle_options(client, callback_query):
-    global BOT, MSG
-
-    if callback_query.data in ["normal", "zip", "unzip", "undzip"]:
-        BOT.Mode.type = callback_query.data
-        await callback_query.message.delete()
-        await colab_bot.delete_messages(
-            chat_id=callback_query.message.chat.id,
-            message_ids=callback_query.message.reply_to_message_id,
-        )
-
-        await colab_bot.send_chat_action(chat_id=OWNER, action=enums.ChatAction.TYPING)
-
-        BOT.State.task_going = True
-        BOT.State.started = False
-        BotTimes.start_time = datetime.now()
-        event_loop = get_event_loop()
-        BOT.TASK = event_loop.create_task(taskScheduler())  # type: ignore
-        await BOT.TASK
-        BOT.State.task_going = False
-
-    elif callback_query.data == "video":
-        keyboard = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Split Videos", callback_data="split-true"),
-                    InlineKeyboardButton("Zip Videos", callback_data="split-false"),
-                ],
-                [
-                    InlineKeyboardButton("Convert", callback_data="convert-true"),
-                    InlineKeyboardButton(
-                        "Don't Convert", callback_data="convert-false"
-                    ),
-                ],
-                [
-                    InlineKeyboardButton("To » Mp4", callback_data="mp4"),
-                    InlineKeyboardButton("To » Mkv", callback_data="mkv"),
-                ],
-                [
-                    InlineKeyboardButton("High Quality", callback_data="q-High"),
-                    InlineKeyboardButton("Low Quality", callback_data="q-Low"),
-                ],
-                [InlineKeyboardButton("Back ⏎", callback_data="back")],
-            ]
-        )
-        await callback_query.message.edit_text(
-            f"CHOOSE YOUR DESIRED OPTION ⚙️ »\n\n╭⌬ CONVERT » <code>{BOT.Setting.convert_video}</code>\n├⌬ SPLIT » <code>{BOT.Setting.split_video}</code>\n├⌬ OUTPUT FORMAT » <code>{BOT.Options.video_out}</code>\n╰⌬ OUTPUT QUALITY » <code>{BOT.Setting.convert_quality}</code>",
-            reply_markup=keyboard,
-        )
-    elif callback_query.data == "caption":
-        keyboard = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Monospace", callback_data="code-Monospace"),
-                    InlineKeyboardButton("Bold", callback_data="b-Bold"),
-                ],
-                [
-                    InlineKeyboardButton("Italic", callback_data="i-Italic"),
-                    InlineKeyboardButton("Underlined", callback_data="u-Underlined"),
-                ],
-                [InlineKeyboardButton("Regular", callback_data="p-Regular")],
-            ]
-        )
-        await callback_query.message.edit_text(
-            "CHOOSE YOUR CAPTION FONT STYLE »\n\n⌬ <code>Monospace</code>\n⌬ Regular\n⌬ <b>Bold</b>\n⌬ <i>Italic</i>\n⌬ <u>Underlined</u>",
-            reply_markup=keyboard,
-        )
-    elif callback_query.data == "thumb":
-        keyboard = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Delete Thumbnail", callback_data="del-thumb"),
-                ],
-                [
-                    InlineKeyboardButton("Go Back ⏎", callback_data="back"),
-                ],
-            ]
-        )
-        thmb_ = "None" if not BOT.Setting.thumbnail else "Exists"
-        await callback_query.message.edit_text(
-            f"CHOOSE YOUR THUMBNAIL SETTINGS »\n\n⌬ Thumbnail » {thmb_}\n⌬ Send an Image to set as Your Thumbnail",
-            reply_markup=keyboard,
-        )
-    elif callback_query.data == "del-thumb":
-        if BOT.Setting.thumbnail:
-            os.remove(Paths.THMB_PATH)
-        BOT.Setting.thumbnail = False
-        await utils.send_settings(
-            callback_query.message, callback_query.message.id, False
-        )
-    elif callback_query.data == "set-prefix":
-        await callback_query.message.edit_text(
-            "Send a Text to Set as PREFIX by REPLYING THIS MESSAGE »"
-        )
-        BOT.State.prefix = True
-    elif callback_query.data == "set-suffix":
-        await callback_query.message.edit_text(
-            "Send a Text to Set as SUFFIX by REPLYING THIS MESSAGE »"
-        )
-        BOT.State.suffix = True
-    elif callback_query.data in [
-        "code-Monospace",
-        "p-Regular",
-        "b-Bold",
-        "i-Italic",
-        "u-Underlined",
-    ]:
-        res = callback_query.data.split("-")
-        BOT.Options.caption = res[0]
-        BOT.Setting.caption = res[1]
-        await utils.send_settings(
-            callback_query.message, callback_query.message.id, False
-        )
-    elif callback_query.data in ["split-true", "split-false"]:
-        BOT.Options.is_split = True if callback_query.data == "split-true" else False
-        BOT.Setting.split_video = (
-            "Split Videos" if callback_query.data == "split-true" else "Zip Videos"
-        )
-        await utils.send_settings(
-            callback_query.message, callback_query.message.id, False
-        )
-    elif callback_query.data in [
-        "convert-true",
-        "convert-false",
-        "mp4",
-        "mkv",
-        "q-High",
-        "q-Low",
-    ]:
-        if callback_query.data in ["convert-true", "convert-false"]:
-            BOT.Options.convert_video = (
-                True if callback_query.data == "convert-true" else False
-            )
-            BOT.Setting.convert_video = (
-                "Yes" if callback_query.data == "convert-true" else "No"
-            )
-        elif callback_query.data in ["q-High", "q-Low"]:
-            BOT.Setting.convert_quality = callback_query.data.split("-")[-1]
-            BOT.Options.convert_quality = (
-                True if BOT.Setting.convert_quality == "High" else False
-            )
-            await utils.send_settings(
-                callback_query.message, callback_query.message.id, False
-            )
-        else:
-            BOT.Options.video_out = callback_query.data
-        await utils.send_settings(
-            callback_query.message, callback_query.message.id, False
-        )
-    elif callback_query.data in ["media", "document"]:
-        BOT.Options.stream_upload = True if callback_query.data == "media" else False
-        BOT.Setting.stream_upload = (
-            "Media" if callback_query.data == "media" else "Document"
-        )
-        await utils.send_settings(
-            callback_query.message, callback_query.message.id, False
-        )
-
-    elif callback_query.data == "close":
-        await callback_query.message.delete()
-    elif callback_query.data == "back":
-        await utils.send_settings(
-            callback_query.message, callback_query.message.id, False
-        )
-
-    # @main Triggering Actual Leech Functions
-    elif callback_query.data in ["ytdl-true", "ytdl-false"]:
-        BOT.Mode.ytdl = True if callback_query.data == "ytdl-true" else False
-        await callback_query.message.delete()
-        await colab_bot.delete_messages(
-            chat_id=callback_query.message.chat.id,
-            message_ids=callback_query.message.reply_to_message_id,
-        )
-        MSG.status_msg = await colab_bot.send_message(
-            chat_id=OWNER,
-            text="#STARTING_TASK\n\n**Starting your task in a few Seconds...🦐**",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton("Cancel ❌", callback_data="cancel")],
-                ]
-            ),
-        )
-        BOT.State.task_going = True
-        BOT.State.started = False
-        BotTimes.start_time = datetime.now()
-        event_loop = get_event_loop()
-        BOT.TASK = event_loop.create_task(taskScheduler())  # type: ignore
-        await BOT.TASK
-        BOT.State.task_going = False
-
-    # If user Wants to Stop The Task
-    elif callback_query.data == "cancel":
-        await cancelTask("User Cancelled !")
-
-
-@colab_bot.on_message(filters.command("setname") & filters.private)
-async def custom_name(client, message):
-    global BOT
-    if len(message.command) != 2:
-        msg = await message.reply_text(
-            "Send\n/setname <code>custom_fileame.extension</code>\nTo Set Custom File Name 📛",
-            quote=True,
-        )
-    else:
-        BOT.Options.custom_name = message.command[1]
-        msg = await message.reply_text(
-            "Custom Name Has Been Successfully Set !", quote=True
-        )
-
-    await sleep(15)
-    await message_deleter(message, msg)
-
-
-@colab_bot.on_message(filters.command("zipaswd") & filters.private)
-async def zip_pswd(client, message):
-    global BOT
-    if len(message.command) != 2:
-        msg = await message.reply_text(
-            "Send\n/zipaswd <code>password</code>\nTo Set Password for Output Zip File. 🔐",
-            quote=True,
-        )
-    else:
-        BOT.Options.zip_pswd = message.command[1]
-        msg = await message.reply_text(
-            "Zip Password Has Been Successfully Set !", quote=True
-        )
-
-    await sleep(15)
-    await message_deleter(message, msg)
-
-
-@colab_bot.on_message(filters.command("unzipaswd") & filters.private)
-async def unzip_pswd(client, message):
-    global BOT
-    if len(message.command) != 2:
-        msg = await message.reply_text(
-            "Send\n/unzipaswd <code>password</code>\nTo Set Password for Extracting Archives. 🔓",
-            quote=True,
-        )
-    else:
-        BOT.Options.unzip_pswd = message.command[1]
-        msg = await message.reply_text(
-            "Unzip Password Has Been Successfully Set !", quote=True
-        )
-
-    await sleep(15)
-    await message_deleter(message, msg)
 
 
 logging.info("Colab Leecher Started !")
---
+++ colab_leecher/handlers/callbacks.py
@@ -0,0 +1,205 @@
+# copyright 2024 © Xron Trix | https://github.com/XronTrix10
+
+import os
+from pyrogram import enums
+from datetime import datetime
+from asyncio import get_event_loop
+from colab_leecher import colab_bot, OWNER
+from colab_leecher.utility.handler import cancelTask
+from colab_leecher.utility.task_manager import taskScheduler
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from colab_leecher.utility.variables import BOT, MSG, BotTimes, Paths
+
+async def handle_callbacks(callback_query):
+    global BOT, MSG
+
+    if callback_query.data in ["normal", "zip", "unzip", "undzip"]:
+        BOT.Mode.type = callback_query.data
+        await callback_query.message.delete()
+        await colab_bot.delete_messages(
+            chat_id=callback_query.message.chat.id,
+            message_ids=callback_query.message.reply_to_message_id,
+        )
+
+        await colab_bot.send_chat_action(chat_id=OWNER, action=enums.ChatAction.TYPING)
+
+        BOT.State.task_going = True
+        BOT.State.started = False
+        BotTimes.start_time = datetime.now()
+        event_loop = get_event_loop()
+        BOT.TASK = event_loop.create_task(taskScheduler())  # type: ignore
+        await BOT.TASK
+        BOT.State.task_going = False
+
+    elif callback_query.data == "video":
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Split Videos", callback_data="split-true"),
+                    InlineKeyboardButton("Zip Videos", callback_data="split-false"),
+                ],
+                [
+                    InlineKeyboardButton("Convert", callback_data="convert-true"),
+                    InlineKeyboardButton(
+                        "Don't Convert", callback_data="convert-false"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton("To » Mp4", callback_data="mp4"),
+                    InlineKeyboardButton("To » Mkv", callback_data="mkv"),
+                ],
+                [
+                    InlineKeyboardButton("High Quality", callback_data="q-High"),
+                    InlineKeyboardButton("Low Quality", callback_data="q-Low"),
+                ],
+                [InlineKeyboardButton("Back ⏎", callback_data="back")],
+            ]
+        )
+        await callback_query.message.edit_text(
+            f"CHOOSE YOUR DESIRED OPTION ⚙️ »\n\n╭⌬ CONVERT » <code>{BOT.Setting.convert_video}</code>\n├⌬ SPLIT » <code>{BOT.Setting.split_video}</code>\n├⌬ OUTPUT FORMAT » <code>{BOT.Options.video_out}</code>\n╰⌬ OUTPUT QUALITY » <code>{BOT.Setting.convert_quality}</code>",
+            reply_markup=keyboard,
+        )
+    elif callback_query.data == "caption":
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Monospace", callback_data="code-Monospace"),
+                    InlineKeyboardButton("Bold", callback_data="b-Bold"),
+                ],
+                [
+                    InlineKeyboardButton("Italic", callback_data="i-Italic"),
+                    InlineKeyboardButton("Underlined", callback_data="u-Underlined"),
+                ],
+                [InlineKeyboardButton("Regular", callback_data="p-Regular")],
+            ]
+        )
+        await callback_query.message.edit_text(
+            "CHOOSE YOUR CAPTION FONT STYLE »\n\n⌬ <code>Monospace</code>\n⌬ Regular\n⌬ <b>Bold</b>\n⌬ <i>Italic</i>\n⌬ <u>Underlined</u>",
+            reply_markup=keyboard,
+        )
+    elif callback_query.data == "thumb":
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Delete Thumbnail", callback_data="del-thumb"),
+                ],
+                [
+                    InlineKeyboardButton("Go Back ⏎", callback_data="back"),
+                ],
+            ]
+        )
+        thmb_ = "None" if not BOT.Setting.thumbnail else "Exists"
+        await callback_query.message.edit_text(
+            f"CHOOSE YOUR THUMBNAIL SETTINGS »\n\n⌬ Thumbnail » {thmb_}\n⌬ Send an Image to set as Your Thumbnail",
+            reply_markup=keyboard,
+        )
+    elif callback_query.data == "del-thumb":
+        if BOT.Setting.thumbnail:
+            os.remove(Paths.THMB_PATH)
+        BOT.Setting.thumbnail = False
+        await utils.send_settings(
+            callback_query.message, callback_query.message.id, False
+        )
+    elif callback_query.data == "set-prefix":
+        await callback_query.message.edit_text(
+            "Send a Text to Set as PREFIX by REPLYING THIS MESSAGE »"
+        )
+        BOT.State.prefix = True
+    elif callback_query.data == "set-suffix":
+        await callback_query.message.edit_text(
+            "Send a Text to Set as SUFFIX by REPLYING THIS MESSAGE »"
+        )
+        BOT.State.suffix = True
+    elif callback_query.data in [
+        "code-Monospace",
+        "p-Regular",
+        "b-Bold",
+        "i-Italic",
+        "u-Underlined",
+    ]:
+        res = callback_query.data.split("-")
+        BOT.Options.caption = res[0]
+        BOT.Setting.caption = res[1]
+        await utils.send_settings(
+            callback_query.message, callback_query.message.id, False
+        )
+    elif callback_query.data in ["split-true", "split-false"]:
+        BOT.Options.is_split = True if callback_query.data == "split-true" else False
+        BOT.Setting.split_video = (
+            "Split Videos" if callback_query.data == "split-true" else "Zip Videos"
+        )
+        await utils.send_settings(
+            callback_query.message, callback_query.message.id, False
+        )
+    elif callback_query.data in [
+        "convert-true",
+        "convert-false",
+        "mp4",
+        "mkv",
+        "q-High",
+        "q-Low",
+    ]:
+        if callback_query.data in ["convert-true", "convert-false"]:
+            BOT.Options.convert_video = (
+                True if callback_query.data == "convert-true" else False
+            )
+            BOT.Setting.convert_video = (
+                "Yes" if callback_query.data == "convert-true" else "No"
+            )
+        elif callback_query.data in ["q-High", "q-Low"]:
+            BOT.Setting.convert_quality = callback_query.data.split("-")[-1]
+            BOT.Options.convert_quality = (
+                True if BOT.Setting.convert_quality == "High" else False
+            )
+            await utils.send_settings(
+                callback_query.message, callback_query.message.id, False
+            )
+        else:
+            BOT.Options.video_out = callback_query.data
+        await utils.send_settings(
+            callback_query.message, callback_query.message.id, False
+        )
+    elif callback_query.data in ["media", "document"]:
+        BOT.Options.stream_upload = True if callback_query.data == "media" else False
+        BOT.Setting.stream_upload = (
+            "Media" if callback_query.data == "media" else "Document"
+        )
+        await utils.send_settings(
+            callback_query.message, callback_query.message.id, False
+        )
+
+    elif callback_query.data == "close":
+        await callback_query.message.delete()
+    elif callback_query.data == "back":
+        await utils.send_settings(
+            callback_query.message, callback_query.message.id, False
+        )
+
+    # @main Triggering Actual Leech Functions
+    elif callback_query.data in ["ytdl-true", "ytdl-false"]:
+        BOT.Mode.ytdl = True if callback_query.data == "ytdl-true" else False
+        await callback_query.message.delete()
+        await colab_bot.delete_messages(
+            chat_id=callback_query.message.chat.id,
+            message_ids=callback_query.message.reply_to_message_id,
+        )
+        MSG.status_msg = await colab_bot.send_message(
+            chat_id=OWNER,
+            text="#STARTING_TASK\n\n**Starting your task in a few Seconds...🦐**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("Cancel ❌", callback_data="cancel")],
+                ]
+            ),
+        )
+        BOT.State.task_going = True
+        BOT.State.started = False
+        BotTimes.start_time = datetime.now()
+        event_loop = get_event_loop()
+        BOT.TASK = event_loop.create_task(taskScheduler())  # type: ignore
+        await BOT.TASK
+        BOT.State.task_going = False
+
+    # If user Wants to Stop The Task
+    elif callback_query.data == "cancel":
+        await cancelTask("User Cancelled !")
