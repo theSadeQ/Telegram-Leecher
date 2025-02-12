940569a94b3bc4e879543992ec19501d119fe91e
theSadeQ <100784449+theSadeQ@users.noreply.github.com>
2025-02-12 23:20:08 +0330

    __init__.py

--- colab_leecher/__init__.py
+++ colab_leecher/__init__.py
@@ -20,3 +20,5 @@ logging.basicConfig(level=logging.INFO)
 install()
 
 colab_bot = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+# Add this line to define BOT
+BOT = colab_bot
